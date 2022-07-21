972a678f7b810af2cdef832d911c07820d348ec1
Piyush Agarwal <70555785+piyushag0611@users.noreply.github.com>
2022-07-22 00:23:39 +0530

    Update generate.py

--- Symmetric Matric Ensembles/generate.py
+++ Symmetric Matric Ensembles/generate.py
@@ -3,14 +3,32 @@ import matplotlib.pyplot as plt
 from mayavi import mlab
 from sklearn import datasets
 
-def generate_GOE(n, d)
-"""
-generate_GOE returns a sample of 'n' symmetric matrices ~ GOE(d,d)
-"""
+def generate_GOE(n=100, d=2)
+    """
+    Returns a sample of 'n' symmetric matrices ~ GOE(d,d)
+
+    Parameters
+    ----------
+    n : int
+      Size of the sample output. The default is 100.
+    d : int
+      Size of the Symmetric Matrix in the sample. The default is 2.
+      
+    Returns
+    -------
+    matrix_samples : array-like
+                   A list of the d*d symmetric matrices generated
+                   
+    """
     matrix_samples = []
     x = y = z = np.array([])
     for i in range(n):
         B = np.random.normal(size=(d, d))
         A = (B+B.T)/2
         matrix_samples.append(A)
-    
+    return matrix_samples
+
+def generate_uni(n, d, a, b)
+"""
+
+"""
